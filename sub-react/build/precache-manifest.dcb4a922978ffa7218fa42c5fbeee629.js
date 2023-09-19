7237ac35d3d14880c8ed015d38f8d3c4dc9c91ee
giscafer <giscafer@outlook.com>
2023-09-19 17:02:11 +0800

    chore: 提交构建产物

---
+++ sub-react/build/precache-manifest.dcb4a922978ffa7218fa42c5fbeee629.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4db381a65161d57852a17731dcb413c8",
+    "url": "/sub-reactindex.html"
+  },
+  {
+    "revision": "5488508bc0399dbe9b06",
+    "url": "/sub-reactstatic/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3f19ba4f410ba60c4a5d",
+    "url": "/sub-reactstatic/js/2.020851bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sub-reactstatic/js/2.020851bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5488508bc0399dbe9b06",
+    "url": "/sub-reactstatic/js/main.8df75a92.chunk.js"
+  },
+  {
+    "revision": "21deb12e4b75e470b86f",
+    "url": "/sub-reactstatic/js/runtime-main.eeea57a7.js"
+  }
+]);
