a62c1683c579fd091e61701d0a1d6f00932f3501
giscafer <giscafer@outlook.com>
2023-09-19 16:03:52 +0800

    chore: fix error

--- sub-vue/common/src/store/global-register.js
+++ sub-vue/common/src/store/global-register.js
@@ -1,20 +1,20 @@
+/* eslint-disable no-mixed-operators */
 
 /**
- * 
- * @param {vuex实例} store 
- * @param {qiankun下发的props} props 
+ *
+ * @param {vuex实例} store
+ * @param {qiankun下发的props} props
  */
 function registerGlobalModule (store, props = {}) {
-
   if (!store || !store.hasModule) {
-    return;
+    return
   }
 
   // 获取初始化的state
   const initState = props.getGlobalState && props.getGlobalState() || {
     menu: [],
     user: {}
-  };
+  }
 
   // 将父应用的数据存储到子应用中，命名空间固定为global
   if (!store.hasModule('global')) {
@@ -24,13 +24,13 @@ function registerGlobalModule (store, props = {}) {
       actions: {
         // 子应用改变state并通知父应用
         setGlobalState ({ commit }, payload) {
-          commit('setGlobalState', payload);
-          commit('emitGlobalState', payload);
+          commit('setGlobalState', payload)
+          commit('emitGlobalState', payload)
         },
         // 初始化，只用于mount时同步父应用的数据
         initGlobalState ({ commit }, payload) {
-          commit('setGlobalState', payload);
-        },
+          commit('setGlobalState', payload)
+        }
       },
       mutations: {
         setGlobalState (state, payload) {
@@ -40,16 +40,16 @@ function registerGlobalModule (store, props = {}) {
         // 通知父应用
         emitGlobalState (state) {
           if (props.setGlobalState) {
-            props.setGlobalState(state);
+            props.setGlobalState(state)
           }
-        },
-      },
-    };
-    store.registerModule('global', globalModule);
+        }
+      }
+    }
+    store.registerModule('global', globalModule)
   } else {
     // 每次mount时，都同步一次父应用数据
-    store.dispatch('global/initGlobalState', initState);
+    store.dispatch('global/initGlobalState', initState)
   }
 };
 
-export default registerGlobalModule;
+export default registerGlobalModule
